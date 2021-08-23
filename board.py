7d69af16d1d1cd1cafc22c2a6da6dc841e23ba37
gonsis10 <simoonsong1006@gmail.com>
2021-08-22 22:19:20 -0700

    Committed progress update.

--- board.py
+++ board.py
@@ -17,13 +17,20 @@ class Board:
         return board
 
     def __setup(self, board, players) -> list:
+        def return_type_object(pieces) -> object:
+            for piece in pieces:
+                if
+            return
+
         def return_back_row(pieces) -> list:
             back_row = []
             order = {0: Rook, 1: Knight, 2: Bishop, 3: Queen,
                      4: King, 5: Bishop, 6: Knight, 7: Rook}
             for index in range(len(pieces)):
-                if pieces[index] != order.get(index):
-                    back_row.append(pieces.pop(order.get(index)))
+                if not isinstance(pieces[index], order.get(index)):
+                    piece = return_type_object(pieces)
+                    pieces.remove(return)
+                    back_row.append()
             return pieces
 
         board[0] = return_back_row(players[0].pieces)
