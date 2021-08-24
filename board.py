6cbe457832c6583524ce497c0c1a19ea07d0d3e3
gonsis10 <simoonsong1006@gmail.com>
2021-08-24 16:59:06 -0700

    Committed progress update.

--- board.py
+++ board.py
@@ -13,25 +13,33 @@ class Board:
         self.board = self.__setup(frame, players)
 
     def __str__(self) -> str:
-        board = [[f"{unit} " for unit in row] and "\n" for row in self.board]
+        board = ""
+        for row in range(len(self.board)):
+            board += f"{row + 1} "
+            for column in range(len(self.board[row])):
+                board += f"{self.board[row][column]} "
+            board += "\n"
+        board += "  a b c d e f g h"
         return board
 
     def __setup(self, board, players) -> list:
-        def return_type_object(pieces) -> object:
+        def return_type_object(pieces, value) -> object:
             for piece in pieces:
-                if
-            return
+                if isinstance(piece, value):
+                    return piece
+            return None
 
         def return_back_row(pieces) -> list:
             back_row = []
             order = {0: Rook, 1: Knight, 2: Bishop, 3: Queen,
                      4: King, 5: Bishop, 6: Knight, 7: Rook}
             for index in range(len(pieces)):
-                if not isinstance(pieces[index], order.get(index)):
-                    piece = return_type_object(pieces)
-                    pieces.remove(return)
-                    back_row.append()
-            return pieces
+                supposed_value = order.get(index)
+                if not isinstance(pieces[index], supposed_value):
+                    piece = return_type_object(pieces, supposed_value)
+                    pieces.remove(piece)
+                    back_row.append(piece)
+            return back_row
 
         board[0] = return_back_row(players[0].pieces)
         return board
